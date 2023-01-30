cb8c53636fc53085b3c69555e81e4b21b2b78dba
Tarun Soni <soni.tarun26@gmail.com>
2023-01-31 00:22:07 +0530

    fixed motion for about me

--- src/components/sections/HomeSection/index.tsx
+++ src/components/sections/HomeSection/index.tsx
@@ -1,9 +1,21 @@
 import { fonts } from '@/utils/fonts'
 import Balancer from 'react-wrap-balancer'
+import { motion } from 'framer-motion'
 
 function HomeSection() {
   return (
-    <h3
+    <motion.h3
+      initial={{
+        y: 30,
+        opacity: 0,
+      }}
+      animate={{
+        y: 0,
+        opacity: 1,
+      }}
+      transition={{
+        duration: 0.3,
+      }}
       className={`text-2xl  font-medium mb-6 sm:mb-10 w-full text-gray-200 tracking-tight ${fonts.lexend} px-2`}>
       {`Hi there! I'm Tarun Soni, I make stuff for the web, and mobile.`}
       <br />
@@ -11,7 +23,7 @@ function HomeSection() {
       <Balancer>
         {`I make React apps, React Native apps, and maybe some other stuff too.`}
       </Balancer>
-    </h3>
+    </motion.h3>
   )
 }
 export default HomeSection
