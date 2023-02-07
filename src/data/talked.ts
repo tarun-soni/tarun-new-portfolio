c90b1af30da412683b62207e5740a03fcd0348f6
Tarun Soni <soni.tarun26@gmail.com>
2023-02-07 23:18:21 +0530

    added talked data

--- src/data/talked.ts
+++ src/data/talked.ts
@@ -5,6 +5,10 @@ const talkedData: MadeData[] = [
     title: 'The state of expo ecosystem in 2022',
     liveURL: 'https://www.youtube.com/live/SpuLPE9bVG8?feature=share&t=5842',
   },
+  {
+    title: 'Progressive Web Apps | The GeekyAnts Talk Show',
+    liveURL: 'https://www.youtube.com/watch?v=4pBrgaFoWGY',
+  },
 ]
 
 export default talkedData
