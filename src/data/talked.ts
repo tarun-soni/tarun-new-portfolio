aeceb66ffd203bb96b330a6fcc0ffc26a810875a
Tarun Soni <soni.tarun26@gmail.com>
2023-04-04 01:18:59 +0530

    added epi-1 in talked

--- src/data/talked.ts
+++ src/data/talked.ts
@@ -2,6 +2,10 @@ import { MadeData } from '@/types'
 
 const talkedData: MadeData[] = [
   {
+    title: `Boosting Your React Native Apps' Performance | React Native Rundown`,
+    liveURL: 'https://www.youtube.com/watch?v=5wkA3wrk_wM',
+  },
+  {
     title: 'The state of expo ecosystem in 2022',
     liveURL: 'https://www.youtube.com/live/SpuLPE9bVG8?feature=share&t=5842',
   },
