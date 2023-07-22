16e6dbf64fdd916043e60094ee742da5ba480ba5
Tarun Soni <soni.tarun26@gmail.com>
2023-07-22 19:42:40 +0530

    moved to simple card

--- src/components/CommonCard/index.tsx
+++ src/components/CommonCard/index.tsx
@@ -4,6 +4,8 @@ import { CardProps, MadeData } from '@/types'
 import { fonts } from '@/utils/fonts'
 import useViewport from '@/hooks/useViewPort'
 import getFontClasses from '@/utils/getFontClasses'
+import Container from '../Container'
+import { DiagonalArrow } from '../Icons'
 
 const gradients = [
   {
@@ -13,47 +15,61 @@ const gradients = [
   },
 ]
 
-const generatedGradientIndex = Math.floor(Math.random() * gradients.length)
-
 const CommonCard: FC<CardProps<MadeData>> = ({ data }) => {
   const viewPort = useViewport()
   const fontClasses = getFontClasses(viewPort)
   return (
-    <motion.div
-      onClick={() => window.open(data.liveURL, '_blank')}
-      initial={{
-        y: 30,
-        opacity: 0,
-      }}
-      animate={{
-        y: 0,
-        opacity: 1,
-      }}
-      transition={{
-        duration: 0.3,
-      }}
-      whileHover={{
-        scale: 1.07,
-      }}
-      key={data.title}
-      className={`
-      ${fontClasses}
-        rounded-md m-4 flex-wrap h-32 w-72
-        bg-gradient-to-r px-2 py-2 
-        cursor-pointer
-        ${gradients[generatedGradientIndex].from}
-        ${gradients[generatedGradientIndex].via}
-        ${gradients[generatedGradientIndex].to}
- border-gradient-to-r
+    <Container>
+      <motion.div
+        onClick={() => window.open(data.liveURL, '_blank')}
+        initial={{
+          y: 30,
+          opacity: 0,
+        }}
+        animate={{
+          y: 0,
+          opacity: 1,
+        }}
+        transition={{
+          duration: 0.1,
+        }}
+        whileHover={{
+          scale: 1.02,
+        }}
+        key={data.title}
+        className={`
+          ${fontClasses}
+          cursor-pointer
+          rounded-md   
+          w-98
+          m-3
         `}>
-      <div className="flex flex-col justify-between h-full bg-stone-900  rounded-md p-4">
-        <h4
-          className={`${fontClasses} text-lg md:text-lg font-medium mb-6 sm:mb-10 w-full light:text-white-800 dark:text-white-100 tracking-tight ${fonts.inter}`}>
-          {data.title}
-        </h4>
-      </div>
-    </motion.div>
+        <div
+          className="flex flex-col justify-between h-full bg-stone-900  rounded-md p-4 
+          w-98
+        ">
+          <h4
+            className={` text-base font-medium text-white-100 tracking-tight ${fonts.inter}`}>
+            {data.title}
+          </h4>
+          {/* <CardAction /> */}
+        </div>
+      </motion.div>
+    </Container>
   )
 }
 
 export default CommonCard
+
+const CardAction = () => (
+  <div className="flex flex-row align-bottom mt-4">
+    <div
+      className={`text-sm font-medium text-white-100 tracking-tight ${fonts.inter}`}>
+      Read
+    </div>
+
+    <div className="items-center">
+      <DiagonalArrow />
+    </div>
+  </div>
+)
