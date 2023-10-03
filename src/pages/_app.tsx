e87a8ae0cf77c0e0a3e952cff927d47043b4bfe4
Tarun Soni <soni.tarun26@gmail.com>
2023-10-04 00:15:51 +0530

    fixed _app for build issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,20 @@
-import type { AppType } from 'next/dist/shared/lib/utils'
+import { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 import '../styles/globals.css'
 import GoogleAnalytics from '@/components/GoogleAnalytics/GoogleAnalytics'
 import { useRouter } from 'next/router'
 import { RESUME_URL } from '@/data/profileURLs'
 
-const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   const route = useRouter()
 
   const params = route?.query?.redirect
 
   if (params === 'resume') {
     // redirect to RESUME_URL
-    window.location.href = RESUME_URL
+    if (typeof window !== 'undefined') {
+      window.location.href = RESUME_URL
+    }
   }
 
   return (
