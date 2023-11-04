b43e3e29e690b5cd6bc11a01ccc023bfee13dce0
Tarun Soni <soni.tarun26@gmail.com>
2023-11-04 18:52:03 +0530

    🔨- fix deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import type { AppType } from 'next/dist/shared/lib/utils'
+import { AppType } from 'next/dist/shared/lib/utils'
 import { Analytics } from '@vercel/analytics/react'
 import '../styles/globals.css'
 import GoogleAnalytics from '@/components/GoogleAnalytics/GoogleAnalytics'
 import HydrationWrapper from '@/components/Hydration'
+import { AppProps } from 'next/app'
 
-const MyApp: AppType = ({ Component, pageProps }) => (
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => (
   <HydrationWrapper>
     <Component {...pageProps} />
     <Analytics />
