66819cc4a578e58f1f35f50f258a18578b888417
Tarun Soni <soni.tarun26@gmail.com>
2024-09-08 23:24:54 +0530

    updated resume link

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,5 @@
 const RESUME_URL =
-  'https://drive.google.com/file/d/13MIYS8q8lb2pAN5FdNAEUblvYRTQCXFk/view?usp=drive_link'
+  'https://drive.google.com/file/d/1n_efzsWRChwoNIWkfZ6_b2BUD8G0Eqjg/view?usp=sharing'
 const ICON_BUTTON_COMMON_STYLES =
   'mr-1 w-30 h-30 rounded-md p-2 text-lg dark:hover:bg-gray-700 hover:bg-gray-300'
 
