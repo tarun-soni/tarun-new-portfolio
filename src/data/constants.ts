2140abc8bec14f1a5e03afa22a8da23a623a2a91
Tarun Soni <33228915+tarun-soni@users.noreply.github.com>
2024-09-14 17:58:31 +0530

    Update constants.ts with resume

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,5 @@
 const RESUME_URL =
-  'https://drive.google.com/file/d/1n_efzsWRChwoNIWkfZ6_b2BUD8G0Eqjg/view?usp=sharing'
+  'https://drive.google.com/file/d/1ycCo6aSWM_5RvJ4nApAErG7MWwh0dC-1/view?usp=sharing'
 const ICON_BUTTON_COMMON_STYLES =
   'mr-1 w-30 h-30 rounded-md p-2 text-lg dark:hover:bg-gray-700 hover:bg-gray-300'
 
