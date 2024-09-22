80c0e5bb03b171b1520e64d38f0bf2664fe30b6f
Tarun Soni <soni.tarun26@gmail.com>
2024-09-22 15:22:21 +0530

    added meta

--- src/head.tsx
+++ src/head.tsx
@@ -1,12 +1,34 @@
+import React from 'react'
+
 export default function Head() {
   return (
     <>
-      <title>Tarun {`\u0027`}s portfolio</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-
+      <title>Tarun&apos;s Portfolio</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="description"
         content="This is Tarun Sonis portfolio made to showcase the skills and talks and projects he has done."
+      />
+      <meta name="robots" content="index, follow" />
+      <meta property="og:title" content="Tarun's Portfolio" />
+      <meta
+        property="og:description"
+        content="Showcasing skills, talks, and projects by Tarun Soni."
+      />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/dro3nqmss/image/upload/e_sharpen:50/e_upscale/tarun-new-portfolio/dhlzig9wqsenzywaeibv.jpg"
+      />
+      <meta property="og:url" content="https://tarun.website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Tarun's Portfolio" />
+      <meta
+        name="twitter:description"
+        content="Showcasing skills, talks, and projects by Tarun Soni."
+      />
+      <meta
+        name="twitter:image"
+        content="https://res.cloudinary.com/dro3nqmss/image/upload/e_sharpen:50/e_upscale/tarun-new-portfolio/dhlzig9wqsenzywaeibv.jpg"
       />
       <link rel="icon" href="/favicon.ico" />
     </>
