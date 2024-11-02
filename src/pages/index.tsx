d8c974c9dc674d989ec1c7b39f56182894274bae
Tarun Soni <soni.tarun26@gmail.com>
2024-11-03 01:07:01 +0530

    updated config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,15 +19,13 @@ declare global {
 }
 
 const Main = () => {
-  const IS_DEV = process.env.MODE !== 'development'
+  const IS_DEV = process.env.MODE === 'development'
 
   const amplitudeKey = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY || ''
 
-  console.log('amplitudeKey', amplitudeKey)
   return (
     <TooltipProvider>
       <Home />
-      {/* <script src="https://cdn.amplitude.com/libs/analytics-browser-2.11.7-min.js.gz"></script> */}
 
       {!IS_DEV && (
         <>
