0fd26f751c3da77ce4a2a1ef0b3ac526756f2f56
Tarun Soni <soni.tarun26@gmail.com>
2024-11-03 01:10:56 +0530

    update key

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ declare global {
 const Main = () => {
   const IS_DEV = process.env.MODE === 'development'
 
-  const amplitudeKey = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY || ''
+  const amplitudeKey = process.env.AMPLITUDE || ''
 
   return (
     <TooltipProvider>
